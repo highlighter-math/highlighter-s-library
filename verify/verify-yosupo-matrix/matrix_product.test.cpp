74fc35fa95edfaab4e3cafdad069a1b5d70c2d9c
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2024-06-17 08:27:03 +0900

    Create matrix_product.test.cpp

---
+++ verify/verify-yosupo-matrix/matrix_product.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/matrix_product"
+#include "../../matrix/matrix.hpp"
+#include<iostream>
+#include<atcoder/modint>
+using namespace atcoder;
+
+int main(){
+	int N,M,K;
+	scanf("%d%d%d",&N,&M,&K);
+	matrix<mint> A(N,M);
+	matrix<mint> B(M,K);
+	for(int i=0;i<N;i++){
+		for(int j=0;j<M;j++){
+			int a;
+			scanf("%d",&a);
+			A[i][j]=a;
+		}
+	}
+	for(int i=0;i<M;i++){
+		for(int j=0;j<K;j++){
+			int b;
+			scanf("%d",&b);
+			B[i][j]=b;
+		}
+	}
+	A*=B;
+	for(int i=0;i<A.H;i++){
+		for(int j=0;j<A.W;j++){
+			printf("%d ",A[i][j].val());
+		}
+		printf("\n");
+	}
+}
