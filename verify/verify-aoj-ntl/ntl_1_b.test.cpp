85c087d24d289581d05457d5bd8bd2d09b23b4d8
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2024-06-18 19:24:08 +0900

    Update ntl_1_b.test.cpp

--- verify/verify-aoj-ntl/ntl_1_b.test.cpp
+++ verify/verify-aoj-ntl/ntl_1_b.test.cpp
@@ -1,22 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/NTL_1_B"
-#include<functional>
 #include<stdio.h>
-
-template<class S,auto op,auto e>
-S power(S a,long long b){
-	static_assert(std::is_convertible_v<decltype(op), std::function<S(S, S)>>,"op must work as S(S, S)");
-	static_assert(std::is_convertible_v<decltype(e), std::function<S()>>,"e must work as S()");
-    S mul=a;
-    S ans=e();
-    while(b){
-        if(b&1){
-        	ans=op(ans,mul);
-        }
-        b>>=1;
-        mul=op(mul,mul);
-    }
-    return ans;
-}
+#include "../../math/power.hpp"
 
 long long p;
 long long op(long long a,long long b){
