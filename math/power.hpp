9e14deb5f2dd080a4a5277c698522eee38d88503
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2024-06-18 23:24:20 +0900

    Update power.hpp

--- math/power.hpp
+++ math/power.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include<functional>
 
-template<class S,auto op,auto e>
-S power(S a,long long b){
+template<class S>
+S power(S a,long long b,S (*op)(S,S),S (*e)()){
 	static_assert(std::is_convertible_v<decltype(op), std::function<S(S, S)>>,"op must work as S(S, S)");
 	static_assert(std::is_convertible_v<decltype(e), std::function<S()>>,"e must work as S()");
     S mul=a;
