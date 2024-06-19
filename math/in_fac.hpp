3d29ca6a27a92ada86333823677e8b38d7a42c5e
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2024-06-20 00:23:25 +0900

    Create in_fac.hpp

---
+++ math/in_fac.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include<vector>
+
+template<class S>
+void in_fac(auto *factorial){
+	factorial[0]=1;
+	for(int i=1;i<10;i++){
+		factorial[i]=factorial[i-1]*i;
+	}
+}
+
+template<class S>
+void in_fac(std::vector<S> &factorial){
+	factorial[0]=1;
+	for(int i=1;i<10;i++){
+		factorial[i]=factorial[i-1]*i;
+	}
+}
