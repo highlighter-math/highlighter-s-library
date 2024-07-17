8800e656b279a438181eb573cab6b9e96d729426
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2024-07-17 20:55:55 +0900

    Update ntl_1_b.test.cpp

--- verify/verify-aoj-ntl/ntl_1_b.test.cpp
+++ verify/verify-aoj-ntl/ntl_1_b.test.cpp
@@ -4,16 +4,8 @@
 
 long long p=1000000007LL;
 
-long long op(long long a,long long b){
-	return a%p*b%p;
-}
-
-long long e(){
-	return 1LL;
-}
-
 int main(){
 	long long a,b;
 	scanf("%lld%lld",&a,&b);
-	printf("%lld\n",power<long long>(a,b,op,e));
+	printf("%lld\n",power(a,b,p));
 }
